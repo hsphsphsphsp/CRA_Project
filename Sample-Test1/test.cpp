a658afc197ad624ee4517dc338d1fefe998660aa
tamongbo <tamongbo@gmail.com>
2024-05-22 13:59:50 +0900

    [Feature] Read LBA never been written

--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -1,14 +1,17 @@
 #include "gmock/gmock.h"
 #include "gtest/gtest.h"
-#include "../TeamProject_SSD/ssd.h"
+#include "../TeamProject_SSD/ssd.cpp"
 
-class MockSsd : public Ssd {
+class MockSSD : public SSD {
 public:
 	MOCK_METHOD(unsigned int, Read, (unsigned int), (override));
 	MOCK_METHOD(void, Write, (unsigned int, unsigned int), (override));
 };
 
-TEST(TestCaseName, TestName) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
+TEST(SSDTest, ReadLBANeverBeenWritten) {
+	unsigned int address = 0x0;
+	unsigned int ret = 0x00000000;
+	SSD ssd;
+
+	EXPECT_EQ(ret, ssd.Read(address));
 }
