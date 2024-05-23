5307042ca83da32cba4c1c5ced8be1a75916c033
lee9142669 <lee9142669@naver.com>
2024-05-23 16:03:26 +0900

    [Feat] SheelScriptApp - exit, help method

--- TeamProject_SSD/ShellTestApp.cpp
+++ TeamProject_SSD/ShellTestApp.cpp
@@ -25,11 +25,56 @@ void ShellTestApp::Read(unsigned int nLba) {
 }
 
 void ShellTestApp::Exit() {
-    
+    std::string sInput;
+
+    while (1) {
+        std::cout << "Exit App? (yes, no)" << std::endl;
+        std::cin >> sInput;
+
+        if (sInput == "yes")
+            std::exit(0);
+        else if (sInput == "no")
+            return;
+        else
+            std::cout << "Please type yes or no" << std::endl;
+    }
 }
 
 void ShellTestApp::Help() {
+    std::cout << "------------ Shell Test App ------------" << std::endl;
 
+    std::cout << "* Specs ********************************" << std::endl;
+    std::cout << "* Max number of LBA = " << dec << pSsd->GetSSDSize() << std::endl;
+
+    std::cout<< std::endl;
+    std::cout << "* Commands *****************************" << std::endl;
+    
+    std::cout << "- write [lba: number] [data: number]" << std::endl;
+    std::cout << "\tWrite data to NAND at LBA number BLOCK" << std::endl;
+
+    std::cout << "- fullwrite [data: number]" << std::endl;
+    std::cout << "\tWrite data to all NAND BLOCK" << std::endl;
+
+    std::cout << "- read [lba: number]" << std::endl;
+    std::cout << "\tRead data from NAND at LBA number BLOCK" << std::endl;
+
+    std::cout << "- fullread" << std::endl;
+    std::cout << "\tRead all data in NAND and Print" << std::endl;
+
+    std::cout << "- exit" << std::endl;
+    std::cout << "\tExit this program" << std::endl;
+
+    std::cout << "- testapp1" << std::endl;
+    std::cout << "\tDo below Test Sequence" << std::endl;
+    std::cout << "\tfullwrite > fullread" << std::endl;
+    std::cout << "\tCheck is correctly writed" << std::endl;
+
+    std::cout << "- testapp2" << std::endl;
+    std::cout << "\tDo below Test Sequence" << std::endl;
+    std::cout << "\tWrite 0xAAAABBBB to 0~5 LBA" << std::endl;
+    std::cout << "\tOver Write 0x12345678 to 0~5 LBA" << std::endl;
+    std::cout << "\tRead 0~5 LBA" << std::endl;
+    std::cout << "\tCheck is 0~5 LBA data overwritted" << std::endl;
 }
 
 void ShellTestApp::FullWrite(unsigned int nData) {
@@ -48,13 +93,14 @@ void ShellTestApp::FullRead() {
 
 void ShellTestApp::DoScript(std::string sTestScriptName) {
     testScript = TestScriptFactory::createScript(sTestScriptName, *pSsd);
-    
+
     if (testScript == nullptr)
     {
         throw exception("INVALID SCRIPT NAME");
     }
     testScript->DoScript();
 }
+
 int ShellTestApp::GetSsdSize() {
     return pSsd->GetSSDSize();
 }
