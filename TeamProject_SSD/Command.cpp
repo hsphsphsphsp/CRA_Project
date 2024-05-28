6a7c2f3159f40bedb9deb6d3e4098079ae4e4c5c
chhy.son <son4934g@gmail.com>
2024-05-28 12:06:19 +0900

    [Feat] Add RunList Command

--- TeamProject_SSD/Command.cpp
+++ TeamProject_SSD/Command.cpp
@@ -97,12 +97,12 @@ void HelpCommand::execute()
     std::cout << "- exit" << std::endl;
     std::cout << "\tExit this program" << std::endl;
 
-    std::cout << "- testapp1" << std::endl;
+    std::cout << "- testscriptapp1" << std::endl;
     std::cout << "\tDo below Test Sequence" << std::endl;
     std::cout << "\tfullwrite > fullread" << std::endl;
     std::cout << "\tCheck is correctly writed" << std::endl;
 
-    std::cout << "- testapp2" << std::endl;
+    std::cout << "- testscriptapp2" << std::endl;
     std::cout << "\tDo below Test Sequence" << std::endl;
     std::cout << "\tWrite 0xAAAABBBB to 0~5 LBA" << std::endl;
     std::cout << "\tOver Write 0x12345678 to 0~5 LBA" << std::endl;
@@ -173,12 +173,17 @@ void EraseCommand::execute()
     pSsd->Erase(nLba, nSize);
 }
 
-RunListCommmand::RunListCommmand(SSD* pSsd, std::string sFilename) :
+RunListCommmand::RunListCommmand(SSD* pSsd, std::string sFileName) :
     Command(pSsd, "Run List"),
-    sFilename{sFilename}
+    sFileName{sFileName}
 {
 }
 
 void RunListCommmand::execute()
 {
+    Runner runner(pSsd);
+
+    if (!runner.CheckRunListFileExist(sFileName))
+        throw std::exception("List file is not exist");
+    runner.DoRunnerTestScenario();
 }
